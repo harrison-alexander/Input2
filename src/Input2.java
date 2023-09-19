dad1113b71132c44a16ce9e6440560bdba3f944f
100142946 <benjamin.alexander7168@gmail.com>
2023-09-19 09:19:50 -0500

    Final

--- src/Input2.java
+++ src/Input2.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Input2 {
     public static void main(String[] args)
     {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter your first name :: ");
+        String firstname = keyboard.nextLine();
+
+        System.out.print("Enter your last name :: ");
+        String lastname = keyboard.nextLine();
+
+        System.out.println("####################");
+        System.out.println("####################");
+        System.out.println(firstname+" "+ lastname);
+        System.out.println("####################");
+        System.out.println("####################");
+        System.out.println(lastname+","  + " "+firstname);
+        System.out.println("####################");
+        System.out.println("####################");
+
+
 
     }
 }
